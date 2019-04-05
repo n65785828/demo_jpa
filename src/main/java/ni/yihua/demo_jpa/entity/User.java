3d4385d5b25d57be6c952e4f33e7d67dbeac5956
tom_gbl <helloTom_gbl@niyihua.com>
2019-04-05 20:56:17 +0800

    add a pojo User.java

---
+++ src/main/java/ni/yihua/demo_jpa/entity/User.java
@@ -0,0 +1,61 @@
+package ni.yihua.demo_jpa.entity;
+
+public class User {
+
+    private long id;
+    private String userName;
+    private String password;
+    private int age;
+
+    public User() {
+    }
+
+    public User(long id, String userName, String password, int age) {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+        this.age = age;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}

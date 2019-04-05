10c03f28cd6e15001b30f2e4a86d614d4e2cb792
tom_gbl <helloTom_gbl@niyihua.com>
2019-04-05 21:03:23 +0800

    MODIFY THE FORWARD INDEX ,ADD VISIT TIMES

--- src/main/java/ni/yihua/demo_jpa/controller/pageForwardController.java
+++ src/main/java/ni/yihua/demo_jpa/controller/pageForwardController.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class pageForwardController {
 
+    public static long VISIT_COUNTS=0;
 
     @RequestMapping("/")
     public String forwardIndex(){
+        VISIT_COUNTS++;
         return "index";
     }
 
